115a40c1296dc2bd30585d6b3c1f29864d80449c
Rei Junior <mastereijunior@gmail.com>
2023-07-12 07:57:37 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,155 @@
+# jquery-form-submit
+
+    An ajax form wrapper plugin to ease form submit event.
+
+[![npm version](https://badge.fury.io/js/@masterei/jquery-form-submit.svg)](https://badge.fury.io/js/@masterei/jquery-form-submit)
+![NPM Downloads](https://img.shields.io/npm/dw/@masterei/jquery-form-submit)
+![NPM License](https://img.shields.io/npm/l/@masterei/jquery-form-submit)
+
+## Dependencies
+
+This project requires the following dependencies: `jQuery & PNotify`
+
+```html
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<script src="https://unpkg.com/pnotify@3.2.0/dist/pnotify.js"></script>
+```
+
+## Installation
+
+### npm
+```sh
+$ npm i @masterei/query-form-submit
+```
+
+### cdn
+```html
+<script src="https://unpkg.com/@masterei/jquery-form-submit/form-submit.min.js"></script>
+```
+
+## Usage
+
+### Basic
+```html
+<form id="sample-form" method="POST" data-url="/post.php">
+    <input type="text" name="sample_input">
+</form>
+
+<script>
+    $(function(){
+        $('form#sample-form').formSubmit();
+    });
+</script>
+```
+
+Note:
+1. Server must return an object response with a `message` key value pair. 
+```javascript
+Eg. ['message' => 'Model has been successfully created.']
+```
+
+2. If no response data is being returned, the plugin will use its default messages.
+
+### Options Usage
+```javascript
+$('form#sample-form').formSubmit({
+    url: '/post.php',
+    method: 'POST',
+    data: new FormData($(this)),
+    done: function (){
+        // success callback
+    },
+    fail: function (){
+        // failed callback
+    },
+    always: function (){
+        // execute whether success or fail
+    },
+    ... etc.
+}, 1500); 
+
+// 1500 is the default page reload timeout in ms after a successfully request
+```
+
+Note: You can freely utilize all the ajax shipped configuration and callback functions.
+
+### Additional Callback Events
+```javascript
+beforeRequest: function (form){
+    // triggered before ajax request is started, allows you to modify the form data
+    // accept current element as an argument
+    
+    // Eg. form.find('input[name="sample_input"]').val('new value');
+},
+afterRequest: function (form){
+    // triggered after request is completed, it executes whether success or fail
+    // allows you to modify aftermath form data
+}
+```
+
+### Default Callback Events
+
+```javascript
+done: function (response) {
+    options.notify.notifySuccess(response.message);
+
+    // reload page on post request
+    if(isMethodPost()){
+        setTimeout(function () {
+            window.location.reload();
+        }, reload);
+    }
+},
+fail: function (error) {
+    options.notify.alert.notifyError(error);
+},
+always: function (xhr) {
+    /**
+     * Remove disabled property only if an error occurred
+     * in a post request or if action is a patch or put
+     */
+    if((xhr.status !== undefined && isMethodPost())
+        || (isMethodPatch() || isMethodPut())) {
+        form.find('button[type="submit"]').prop('disabled', false);
+    }
+}
+```
+
+### Automatic method spoofing for media type request
+
+When dealing with restful api, HTML forms do not support `PUT`, `PATCH`, 
+or `DELETE` actions. So, when defining these methods that are called from an HTML form, 
+you will need to add a hidden `_method` field to the form.
+
+The good news is, the plugin automatically inject this hidden field when it detects 
+a form attribute `enctype="multipart/form-data"` included in the form.
+
+Example:
+```html
+<form id="sample-form" method="POST" data-url="/post.php" enctype="multipart/form-data">
+    <input type="file" name="sample_file">
+</form>
+```
+
+### Tested Compatibility
+
+<ul>
+    <li><a href="https://laravel.com/">Laravel</a></li>
+</ul>
+
+### Screenshot
+![](https://github.com/masterei/jquery-form-submit/blob/main/docs/screenshot-01.png?raw=true)
+
+### Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+### License
+
+The Internet Systems Consortium License (ISC). Please see [License File](LICENSE.md) for 
+more information.
+
+### Disclaimer
+
+This plugin was created by the author to address code repetition when developing various 
+projects for its clients. So, use at your own risk.
---
+++ form-submit.js
@@ -0,0 +1,188 @@
+$(function () {
+    $.fn.extend({
+        formSubmit: function (options = {}, reload = 1500){
+            let form = $(this);
+
+            /**
+             * Event handlers
+             * @required Form object.
+             */
+            options = $.extend({
+                notify: {
+                    notifySuccess: function (message, title = 'Success!'){
+                        new PNotify({
+                            title: title,
+                            text: message,
+                            addclass: 'alert alert-styled-right alert-arrow-right',
+                            type: 'success'
+                        });
+                    },
+                    notifyError: function (message, title = 'Error!'){
+                        // object type data
+                        if(message.hasOwnProperty('responseJSON')){
+
+                            if(message.responseJSON.hasOwnProperty('errors')){
+                                for (let key in message.responseJSON.errors) {
+                                    message = message.responseJSON.errors[key][0];
+                                    break;
+                                }
+                            } else if(message.responseJSON.hasOwnProperty('message')){
+                                message = message.responseJSON.message;
+                            }
+
+                        } else if(message.hasOwnProperty('message')) {
+                            message = message.message;
+                        }
+
+                        new PNotify({
+                            title: title,
+                            text: message,
+                            addclass: 'alert alert-styled-right alert-arrow-right',
+                            type: 'error'
+                        });
+                    }
+                },
+                beforeRequest: function (form){
+                    // execute before ajax request
+                    // used current object element to manipulate data
+                },
+                afterRequest: function (form){
+                    // execute after ajax response
+                    // used current object element to manipulate data
+                }
+            }, options);
+
+            // check if form contains file to be uploaded
+            const isEnctypeMultipart = function (){
+                return (typeof options.enctype !== 'undefined' && options.enctype === 'multipart/form-data')
+                    || (typeof form.attr('enctype') !== 'undefined' && form.attr('enctype') === 'multipart/form-data');
+            }
+
+            // url for ajax request
+            const requestEndpoint = function (){
+                return form.attr('action');
+            }
+
+            // method for ajax request
+            const requestMethod = function (){
+                // automatically return 'POST' method if it is multipart/form-data
+                return (typeof form.attr('method') !== 'undefined'
+                    && form.attr('method') !== false)
+                && !isEnctypeMultipart()
+                    ? form.attr('method')
+                    : 'POST';
+            }
+
+            // build request data for ajax request
+            const requestData = function (){
+                let data = isEnctypeMultipart()
+                    ? new FormData(form[0])
+                    : form.serialize();
+
+                // insert method spoofing on multipart/form-data
+                if(isEnctypeMultipart()){
+                    data.append('_method', getActualMethod());
+                }
+
+                return data;
+            }
+
+            // prepare, merge default and user define configs
+            const requestConfigs = function (){
+                let default_configs = {
+                    url: requestEndpoint(),
+                    method: requestMethod(),
+                    data: requestData(),
+                    done: function (response) {
+                        options.notify.notifySuccess(response.message);
+
+                        // reload page on post request
+                        if(isMethodPost()){
+                            setTimeout(function () {
+                                window.location.reload();
+                            }, reload);
+                        }
+                    },
+                    fail: function (error) {
+                        options.notify.alert.notifyError(error);
+                    },
+                    always: function (xhr) {
+                        /**
+                         * Remove disabled property only if an error occurred
+                         * in a post request or if action is a patch or put
+                         */
+                        if((xhr.status !== undefined && isMethodPost())
+                            || (isMethodPatch() || isMethodPut())) {
+                            form.find('button[type="submit"]').prop('disabled', false);
+                        }
+                    }
+                };
+
+                // additional config if form request has files included
+                let multipart_config = {
+                    processData:false,
+                    contentType: false,
+                    cache: false
+                }
+
+                default_configs = isEnctypeMultipart() ? $.extend(multipart_config, default_configs) : default_configs;
+
+                return $.extend(default_configs, options);
+            }
+
+            // get actual method used in request
+            // endpoint method spoofing is included in detection
+            // spoofing method is applicable on multipart/form-data
+            const getActualMethod = function (){
+                return (typeof form.attr('method') !== 'undefined' && form.attr('method') !== false)
+                    ? form.attr('method')
+                    : 'POST';
+            }
+
+            // check if endpoint is post
+            const isMethodPost = function (){
+                return getActualMethod().toLowerCase() === 'post';
+            }
+
+            // check if endpoint is patch
+            const isMethodPatch = function (){
+                return getActualMethod().toLowerCase() === 'patch';
+            }
+
+            // check if endpoint is put
+            const isMethodPut = function (){
+                return getActualMethod().toLowerCase() === 'put';
+            }
+
+            // form submit event
+            form.submit(function (e){
+                // let that = ;
+                e.preventDefault();
+
+                // manipulating form data before request goes here
+                options.beforeRequest(form);
+
+                let configs = requestConfigs();
+
+                // disable submit button
+                $(this).find('button[type="submit"]').prop('disabled', true);
+
+                // execute ajax request
+                $.ajax(configs)
+                    .done(function (response) {
+                        configs.done(response);
+                    }).fail(function (error) {
+                        configs.fail(error);
+                    }).always(function (xhr) {
+                        // enable submit button
+                        configs.always(xhr);
+
+                        // executed after ajax response
+                        options.afterRequest(form);
+                    });
+            });
+
+            return this;
+        }
+    });
+});
